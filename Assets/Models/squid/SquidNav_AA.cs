fe707caa4060d48ce3050e4362eab76b56d6f689
Alex Carbonero <alexcdev@gmail.com>
2012-08-14 18:40:57 -0700

    Started Basic Unity Project
    quit unity before updating files.

---
+++ Assets/Models/squid/SquidNav_AA.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using System.Collections;
+
+public class SquidNav_AA : MonoBehaviour {
+	
+	public GameObject Head;
+	
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		Vector3 myV = new Vector3(1,0,0);
+		this.transform.Translate(myV);
+		Camera myC = this.GetComponent<Camera>();
+		myC.transform.LookAt(this.transform.position);
+		this.GetComponent("turbine");
+	}
+}
