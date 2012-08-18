d7f69b57058325bfa96a565b1caef6a99f08d5cb
Alex Carbonero <alexcdev@gmail.com>
2012-08-18 09:45:45 -0700

    some test updates, nothing important

--- Assets/Scripts/SquidTailMotion_AA.cs
+++ Assets/Scripts/SquidTailMotion_AA.cs
@@ -3,20 +3,33 @@ using System.Collections;
 
 public class SquidTailMotion_AA : MonoBehaviour 
 {
-	public Vector3 parentPos;
-	public Vector3 parentRot;
+	public GameObject parent;
+	public float zOffset;
+	
+	private Quaternion parentLastRot;
+
+	
 	
 	// Use this for initialization
 	void Start ()
 	{
-	
+
+		
 	}
+	
 	
 	// Update is called once per frame
 	void Update () 
 	{
-		parentPos = this.transform.parent.position;
-		parentRot = this.transform.parent.rotation.eulerAngles;
-	
+		
+		Vector3 parentPos =  parent.transform.position;
+		Vector3 parentRot =  parent.transform.rotation.eulerAngles;
+		Vector3 postDelta = new Vector3(0.0f, 0.0f, zOffset);
+		
+		this.transform.position = this.parent.transform.position + postDelta;
+		this.transform.rotation = (this.parentLastRot * this.transform.rotation);
+		
+		parentLastRot = parent.transform.rotation;
+
 	}
 }
